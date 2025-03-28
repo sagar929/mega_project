3f0268032c386cd3dbc429dfe06fb44c9ea3ecf7
sagar929 <sagarshrama130804@gmail.com>
2025-03-28 10:12:32 +0530

    authentication completed

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -3,20 +3,6 @@ import conf from "../conf/conf";
 
 import{Client,Account,ID} from "appwrite"
 
-// const client = new Client()
-// .setEndpoint('https://cloud.appwrite.io/v1')
-
-// .setProject('67e588bb0038dc5c07e8');
-
-
-
-// const account = new Account(client);
-
-// const user = await account.create(
-//     ID.unique(),
-//     'email@example.com',
-//     'password'
-// );
 
 export class AuthService {
     client = new Client();
@@ -31,25 +17,53 @@ export class AuthService {
         //    .setKey(conf.apiKey); 
     }
 
-    async createAccount({email,password,name}){
-        try{
-       const userAccount =   await this.account.create( ID.unique(),email,password,name);
-       
-        if(userAccount){
-            // call another method
-            return userAccount;
-        } else{
-            return  userAccount;
+    async createAccount({ email, password, name }) {
+        try {
+            const userAccount = await this.account.create(ID.unique(), email, password, name);
+    
+            if (userAccount) {
+                
+                return this.login({ email, password });
+            } else {
+                return userAccount;
+            }
+        } catch (error) {
+            throw error; 
         }
-       
-        }
-        catch(error){
+    }
+    
+    async login({ email, password }) {
+        try {
+            return await this.account.createEmailPasswordSession(email, password); // FIXED
+        } catch (error) {
             throw error;
         }
     }
+    
+    async getCurrectUser() { 
+        try {
+            return await this.account.get(); 
+        } catch (error) {
+            console.log("Appwrite service :: getCurrectUser :: error", error);
+            return null; 
+        }
+    }
+    
+    async logout() {
+        try {
+            await this.account.deleteSession("current");
+        } catch (error) {
+            console.log("Appwrite service :: logout :: error", error);
+        }
+    }
+    
+        
+    }
+
+
 
     
-}
+
 
 
 const authService = new AuthService(); 
